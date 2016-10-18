d9f79bbe8e28341a256c795ee9bf1c0e966fd9ee
Shivaprasad Mulky <shevaprasad_d@spanservices.com>
2016-10-18 23:54:49 +0530

    Added Age interpreter #Lesson1

---
+++ Lesson 1/age.py
@@ -0,0 +1,19 @@
+# We'll provide output based on age
+
+# Receive age and store age
+
+age = input('Enter your age: ')
+
+age = int(age)
+
+if (age >= 1) and (age <=18):
+    print("Important Birthday")
+
+elif(age >= 21) and (age <=50):
+    print("Cool Birthday")
+
+elif not(age < 65):
+    print("ok Birthday")
+
+else:
+    print("Sorry, not important")
