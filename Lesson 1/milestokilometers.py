5355f187328b3a98cafd31d1e18cb5280dc1633d
Shivaprasad Mulky <shevaprasad_d@spanservices.com>
2016-10-18 23:38:01 +0530

    Distance converter #Lesson1

---
+++ Lesson 1/milestokilometers.py
@@ -0,0 +1,10 @@
+# Problem : Receive miles and convert to kilometers
+# Kilometers = miles * 1.60934
+
+miles = input( "Enter distance in miles ")
+
+miles = int(miles)
+
+distance = miles * 1.60934
+
+print('Distance of {} miles in Kilometers: {}'.format(miles,distance))
