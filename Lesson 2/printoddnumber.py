1f7a31e715e4d17c8c590f4dc3ccc2e1e3c15966
Shivaprasad Mulky <shevaprasad_d@spanservices.com>
2016-10-20 16:25:31 +0530

    Added Compond Interest #Lesson2

---
+++ Lesson 2/printoddnumber.py
@@ -0,0 +1,10 @@
+# Use for loop through the list from 1 to 21
+for i in range(1, 21):
+    if (i % 2) != 0:
+        print(i)
+    else:
+        exit
+
+        # Use modulus to check the result is NOT EQUAL to 0
+
+        # Print odds
