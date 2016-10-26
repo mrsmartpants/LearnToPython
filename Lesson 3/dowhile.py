dc8e27ada46b05bf22998dcdcfc3291f56071cf3
Shivaprasad Mulky <shevaprasad_d@spanservices.com>
2016-10-27 00:02:12 +0530

    Execption handling in python #Lesson3

--- Lesson 3/dowhile.py
+++ Lesson 3/dowhile.py
@@ -1,3 +1,22 @@
 
 # Do while will execute atleast once
 
+# do
+# Excutes this
+# While Condition
+
+# Guess a number between 1:10
+# You guessed it right
+
+import random
+
+secretno = 7
+
+while True:
+    i = int(input("Enter a number between 1 to 10 "))
+    if i == secretno:
+        print("you guessed right")
+        break
+
+
+
