99ea4d8e0378b4de2a0960032e583f8931989dfa
Shivaprasad Mulky <shevaprasad_d@spanservices.com>
2016-11-01 19:13:38 +0530

    Added math function related files #Lesson3

---
+++ Lesson 3/mathfuctions.py
@@ -0,0 +1,25 @@
+import math
+
+print("ceil(4.4) = ", math.ceil(4.4))
+
+print("floor(4.4) = ", math.floor(4.4))
+
+print("fabs(-4.4) = ", math.fabs(-4.4))
+
+print("factorial(4) = ", math.factorial(4))
+
+print("fmod(5,4) = ", math.fmod(5,4))
+
+print("trunc(4.2) = ", math.trunc(4.2))
+
+print("exp(4) = ", math.exp(4))
+
+print("log(20) = ", math.log(20))
+
+print("log(1000,10) = ", math.log(1000,10))
+
+print("log10(1000) = ", math.log10(1000))
+
+print("degrees(1.57) = ", math.degrees(1.57))
+
+print("radians(90) = ", math.radians(90))
